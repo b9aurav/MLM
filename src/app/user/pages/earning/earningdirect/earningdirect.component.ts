db7538512cc9077b700f81185cc5fe73a0c5723c
Gaurav <hey9aurav@gmail.com>
2023-06-13 12:06:19 +0530

    Direct Earnings for users Implemented

--- src/app/user/pages/earning/earningdirect/earningdirect.component.ts
+++ src/app/user/pages/earning/earningdirect/earningdirect.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service'
+import { environment } from '../../../../../environments/environment';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'EarningDirect',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./earningdirect.component.css']
 })
 export class EarningdirectComponent implements OnInit {
+  user: any;
+  members: any[];
+  directEarningsText: HTMLParagraphElement;
+  referralText: HTMLParagraphElement;
 
-  constructor() { }
+  constructor(private userService: UserService,  private http: HttpClient) { 
+    this.user = this.userService.user
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.getDirectEarnings();
+    }, 100);
+    this.directEarningsText = document.getElementById('directIncomeText') as HTMLParagraphElement;
+    this.referralText = document.getElementById('directMembersText') as HTMLParagraphElement;
+  }
+
+  getDirectEarnings() {
+    var param = {
+      "param": {
+        "user_id": this.userService.user.user_id
+      } 
+    }
+    this.http.post<{ data: any, message: string, referrals: string, earnings: string }>(environment.apiBaseUrl + '/api/GetDirectEarnings', param).subscribe(response => {
+      this.members = response.data;
+      this.directEarningsText.textContent = 'Rs. ' + response.earnings;
+      this.referralText.innerText = response.referrals;
+    }, error => {
+      console.error(error);
+    });
   }
 
 }
