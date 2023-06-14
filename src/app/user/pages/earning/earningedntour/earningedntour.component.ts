ef926a74360796bea62eb844b311c6322cf1866a
Gaurav <hey9aurav@gmail.com>
2023-06-14 19:11:20 +0530

    Education & Tour Implemented

--- src/app/user/pages/earning/earningedntour/earningedntour.component.ts
+++ src/app/user/pages/earning/earningedntour/earningedntour.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service'
+import { environment } from '../../../../../environments/environment';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'EarningEdNTour',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./earningedntour.component.css']
 })
 export class EarningedntourComponent implements OnInit {
+  user: any;
+  rows: any[];
 
-  constructor() { }
+  constructor(private userService: UserService,  private http: HttpClient) {
+    this.user = this.userService.user
+   }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.getEducationTourData();
+    }, 100);
+  }
+
+  getEducationTourData() {
+    var param = {
+      "param": {
+        "user_id": this.userService.user.user_id
+      } 
+    }
+    this.http.post<{ data: any, message: string }>(environment.apiBaseUrl + '/api/GetEduRank', param).subscribe(response => {
+      this.rows = response.data
+    }, error => {
+      console.error(error);
+    });
   }
 
 }
