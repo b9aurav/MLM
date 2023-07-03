b545174263f1cfdf4197190d6ecd5b6f1177d6aa
Gaurav <hey9aurav@gmail.com>
2023-07-03 13:18:23 +0530

    Autopool Earning implemented.

--- src/app/user/pages/earning/earningautopool/earningautopool.component.ts
+++ src/app/user/pages/earning/earningautopool/earningautopool.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service'
+import { environment } from '../../../../../environments/environment';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'EarningAutopool',
-  templateUrl: './earningautopool.component.html',
-  styleUrls: ['./earningautopool.component.css']
+  templateUrl: './Earningautopool.component.html',
+  styleUrls: ['./Earningautopool.component.css']
 })
 export class EarningautopoolComponent implements OnInit {
+  user: any;
+  rows: any[];
 
-  constructor() { }
+  constructor(private userService: UserService,  private http: HttpClient) { 
+    this.user = this.userService.user
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.getAutopoolEarnings();
+    }, 100);
+  }
+
+  getAutopoolEarnings() {
+    var param = {
+      "param": {
+        "user_id": this.userService.user.user_id
+      } 
+    }
+    this.http.post<{ data: any, message: string }>(environment.apiBaseUrl + '/api/GetAutopoolIncome', param).subscribe(response => {
+      this.rows = response.data
+    }, error => {
+      console.error(error);
+    });
   }
 
 }
--- src/app/user/pages/team/autopool/teamautopool.component.ts
+++ src/app/user/pages/team/autopool/teamautopool.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service'
+import { environment } from '../../../../../environments/environment';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'TeamAutopool',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teamautopool.component.css']
 })
 export class TeamAutopoolComponent implements OnInit {
+  user: any;
+  rows: any[];
+  levels: number = 7;
+  members: any[];
+  toggleTable: boolean = false;
+  searchTerm: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UserService,  private http: HttpClient) { 
+    this.user = this.userService.user
+    this.rows = [];
   }
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.getLevelInfo();
+    }, 100);
+  }
+
+  getLevelInfo() {
+    for (let levelRow = 1; levelRow <= this.levels; levelRow++) {
+      setTimeout(() => {
+        var param = {
+          "param": {
+            "user_id": this.userService.user.user_id,
+            "level": levelRow
+          } 
+        }
+        this.http.post<{ data: any, count: string, message: string }>(environment.apiBaseUrl + '/api/GetAutopoolTeamByLevel', param).subscribe(response => {
+          console.log(response)
+          var data = {
+            "level_no": levelRow,
+            "total_members": response.count
+          }
+          this.rows.push(data);
+          this.rows.sort((a, b) => a.level_no - b.level_no);
+        }, error => {
+          console.error(error);
+        });
+      }, 100);
+    }
+  }
+
+  getAutopoolTeamByLevel(level) {
+    var param = {
+      "param": {
+        "user_id": this.userService.user.user_id,
+        "level": level
+      } 
+    }
+    this.http.post<{ data: any, message: string }>(environment.apiBaseUrl + '/api/GetAutopoolTeamByLevel', param).subscribe(response => {
+      this.members = response.data;  
+      this.toggleTable = !this.toggleTable
+    }, error => {
+      console.error(error);
+    });
+  }
 }
