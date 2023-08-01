9737f789095e7bdfe250509f8e8f1acaa1c7868f
Gaurav <hey9aurav@gmail.com>
2023-08-01 15:12:10 +0530

    Profile page bug fix.

--- client/src/app/user/pages/settings/profile/profile.component.ts
+++ client/src/app/user/pages/settings/profile/profile.component.ts
@@ -16,37 +16,51 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
-    } else { 
+    } else {
       var param = {
         "param": {
-          "username": localStorage.getItem('mlm_user')
-        } 
+          "user_id": this.authService.userData.user_id
+        }
       }
-      this.http.post<{ data: any, message: string }>(environment.apiBaseUrl + '/api/GetUserDetails', param).subscribe(response => {
+      this.http.post<{ data: any, message: string }>(environment.apiBaseUrl + '/api/GetUserDetailsForAdmin', param).subscribe(response => {
         let userLabel = document.getElementById("userLabel") as HTMLLabelElement;
-        userLabel.innerText = this.authService.userData.name
+        userLabel.innerText = response.data[0].name
         let nameInput = document.getElementById("nameInput") as HTMLInputElement;
-        nameInput.value = this.authService.userData.name
+        nameInput.value = response.data[0].name
         let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
-        usernameInput.value = this.authService.userData.username
+        usernameInput.value = response.data[0].username
         let useridInput = document.getElementById("useridInput") as HTMLInputElement;
-        useridInput.value = this.authService.userData.user_id
+        useridInput.value = response.data[0].user_id
         let sponsorInput = document.getElementById("sponsorInput") as HTMLInputElement;
-        sponsorInput.value = this.authService.userData.sponsor_id
+        sponsorInput.value = response.data[0].sponsor_id
         let phoneInput = document.getElementById("phoneInput") as HTMLInputElement;
-        phoneInput.value = this.authService.userData.phone
+        phoneInput.value = response.data[0].phone
         let emailInput = document.getElementById("emailInput") as HTMLInputElement;
-        emailInput.value = this.authService.userData.email
+        emailInput.value = response.data[0].email
         let activeInput = document.getElementById("activeInput") as HTMLInputElement;
         let joindateInput = document.getElementById("joindateInput") as HTMLInputElement;
-        joindateInput.value = this.authService.userData.join_date.split('T')[0]
-        if (this.authService.userData.is_active) {
+        joindateInput.value = response.data[0].join_date.split('T')[0]
+        if (response.data[0].is_active) {
           activeInput.value = 'Active'
           let activedateInput = document.getElementById("activedateInput") as HTMLInputElement;
-          activedateInput.value = this.authService.userData.activation_date.split('T')[0]  
+          activedateInput.value = response.data[0].activation_date.split('T')[0]
         } else {
           activeInput.value = 'In-Active'
         }
+        let bankAcInput = document.getElementById("bankAcInput") as HTMLInputElement;
+        bankAcInput.value = response.data[0].ac_no
+        let banknameInput = document.getElementById("banknameInput") as HTMLInputElement;
+        banknameInput.value = response.data[0].bank_name
+        let branchInput = document.getElementById("branchInput") as HTMLInputElement;
+        branchInput.value = response.data[0].branch
+        let ifscInput = document.getElementById("ifscInput") as HTMLInputElement;
+        ifscInput.value = response.data[0].ifsc_code
+        let holderInput = document.getElementById("holderInput") as HTMLInputElement;
+        holderInput.value = response.data[0].bank_ac_holder_name
+        let aadharInput = document.getElementById("aadharInput") as HTMLInputElement;
+        aadharInput.value = response.data[0].aadhar_no
+        let panInput = document.getElementById("panInput") as HTMLInputElement;
+        panInput.value = response.data[0].pan_no
       }, error => console.error(error));
     }
   }
